e93bdc0de2b606787e7289dddc49f1c2fb4ca3ef
I Made Budi Dharma Wipraja <v-i.wipraja@btpn.com>
2023-10-03 20:31:04 +0700

    create login and register

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Navbar from '@/components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -20,7 +22,10 @@ export default function RootLayout({
       <body className={cn(
         'min-h-screen font-sans antialiased grainy',
         inter.className
-      )}>{children}</body>
+      )}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
